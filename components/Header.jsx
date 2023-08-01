355d570e0c81a6cefeb8c0c1dbb2ffec453817c8
Paris Monson <101806410+ParisMonson@users.noreply.github.com>
2023-08-01 16:12:17 +0100

    Made header fixed

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="header">
+    <header className="header fixed top-0 w-full z-50">
       <h1>Badfaith</h1>
     </header>
   );
