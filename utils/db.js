8dd69198f9ee0da7e6184248bbdb143b55f4add0
Paris Monson <101806410+ParisMonson@users.noreply.github.com>
2023-08-03 15:22:57 +0100

    Create get method for all records.

--- utils/db.js
+++ utils/db.js
@@ -7,15 +7,16 @@ const certificate = 'X509-cert-1515475297667010122.pem'
 
 
 export async function saveRecord(data) {
-  console.log("[saveRecord]")
-  await connectToDatabase();
-  console.log("Connected to db!")
+  const db = await connectToDatabase();
+}
+export async function getRecords() {
+  const db = await connectToDatabase();
+  const collection = db.collection("sample_training");
 }
 
 
 async function connectToDatabase() {
   if (cachedDb) {
-    // If a connection exists, we return it.
     return cachedDb;
   }
 
@@ -25,23 +26,17 @@ async function connectToDatabase() {
     serverApi: ServerApiVersion.v1
   });
 
-  let database;
+
+  let db;
 
   try {
     await client.connect();
-    console.log('Connected to database');
-    const database = client.db("badfaith");
-    console.log('Database connected!')
-    const collection = database.collection("badfaith");
-    const docCount = await collection.countDocuments({});
-    console.log(docCount);
-    // perform actions using client
+    db = client.db("badfaith");
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 
-  // Cache the database connection and return it.
-  cachedDb = database;
-  return database;
+  cachedDb = db;
+  return db;
 }
