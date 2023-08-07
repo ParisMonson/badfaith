ce23a0f0fde41dc2f808c2dc2295a70726ed9de8
Paris Monson <101806410+ParisMonson@users.noreply.github.com>
2023-08-07 13:49:16 +0100

    Remove unused import

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Auth0Provider } from "@auth0/auth0-react";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
