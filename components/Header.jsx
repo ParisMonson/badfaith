7a6089cc0b4e3d1fcd739a44aabe968d9e334517
Paris Monson <101806410+ParisMonson@users.noreply.github.com>
2023-08-07 13:56:06 +0100

    Made loginButton dynamic

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,23 @@
+import { useUser } from '@auth0/nextjs-auth0/client';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 
 export default function Header() {
+  const { user, isLoading, error } = useUser();
+
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <header className="header">
       <h1>Badfaith</h1>
-    <LoginButton variant="outlined">Log in</LoginButton>
-    <LogoutButton variant="outlined">Log out</LogoutButton>
+      { !user ? (
+        <LoginButton variant="outlined">Log in</LoginButton>
+      ) : (
+        <LogoutButton variant="outlined">Log out</LogoutButton>
+      )
+      }
     </header>
   );
 };
