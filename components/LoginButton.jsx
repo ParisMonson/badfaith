f2f0b4b5be89a9812826c35bf0e0bee7b4877e5e
Paris Monson <101806410+ParisMonson@users.noreply.github.com>
2023-08-15 11:53:10 +0100

    Edit auth0 callback url

--- components/LoginButton.jsx
+++ components/LoginButton.jsx
@@ -1,14 +1,15 @@
-import Button from '@mui/material/Button';
-import Link from 'next/link';
+import Button from "@mui/material/Button";
+import Link from "next/link";
 import { useEffect } from "react";
 
 export default function LoginButton() {
-
   const handleRedirect = () => {
-    window.location.href = "/api/auth/login";
+    window.location.href = "/api/auth/callback";
   };
 
   return (
-    <Button variant="outlined" onClick={() => handleRedirect()}>Log in</Button>
+    <Button variant="outlined" onClick={() => handleRedirect()}>
+      Log in
+    </Button>
   );
-};
+}
