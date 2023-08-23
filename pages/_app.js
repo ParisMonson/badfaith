2c6edd90145a6b8fe050e7d6732c08fd66d60a5c
Paris Monson <101806410+ParisMonson@users.noreply.github.com>
2023-08-23 11:06:12 +0100

    Configure materialUI for SSR

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,23 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { CacheProvider } from "@emotion/react";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import theme from "../styles/theme";
+import { ThemeProvider } from "@mui/material/styles";
+import createEmotionCache from "../utils/createEmotionCache";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
     </UserProvider>
   );
 }
