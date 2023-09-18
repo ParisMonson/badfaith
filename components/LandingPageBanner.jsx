c0c562478a8c70ef36144aebd0abeb5d63819d59
Paris Monson <parismonson@yahoo.com>
2023-09-18 12:29:31 +0100

    Landing page page structure finished

--- components/LandingPageBanner.jsx
+++ components/LandingPageBanner.jsx
@@ -1,11 +1,16 @@
 export function LandingPageBanner() {
     return (
-      <div className="landing-banner flex flex-row h-[940px] mt-[72px] border-y-2">
-        <div>
-          Section A
+      <div className="landing-banner flex flex-col h-[600px] mt-[72px] border-y-2">
+        <div className="border-2 flex h-[566px]">
+          <div className="w-2/3 border-2 flex">
+            a
+          </div>
+          <div className="border-2 flex w-1/3">
+            b
+          </div>
         </div>
-        <div>
-          Section B
+        <div className="flex border-2 h-[64px]">
+          B
         </div>
       </div>
     );
