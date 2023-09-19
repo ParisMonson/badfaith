0c33bc4469ce840316e4229f26cd4d81c7ec5839
Paris Monson <parismonson@yahoo.com>
2023-09-19 14:26:22 +0100

    Position text

--- components/LandingPageBanner.jsx
+++ components/LandingPageBanner.jsx
@@ -2,8 +2,8 @@ export function LandingPageBanner() {
     return (
       <div className="landing-banner flex flex-col h-[600px] mt-[72px] border-y-2">
         <div className="border-2 flex h-[566px]">
-          <div className="w-2/3 border-2 flex">
-            <div>
+          <div className="w-2/3 border-2 flex flex-col justify-end p-5">
+            <div className="flex">
             <p className="font-HKGroteskRegular text-8xl">
             Ready to analyse some articles?
             </p>
@@ -13,8 +13,8 @@ export function LandingPageBanner() {
             b
           </div>
         </div>
-        <div className="flex flex-row border-2 justify-between">
-          <div className="border-2 w-96 h-[64px]">
+        <div className="flex flex-row border-2 justify-between p-1">
+          <div className="border-2 w-96 h-[64px] font-HKGroteskRegular">
           Available from 18th October
           </div>
           <div className="border-2 w-96 h-[64px]">
