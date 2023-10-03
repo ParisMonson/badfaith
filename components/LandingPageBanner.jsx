d28451ce50f0ac9b78fed0e8a34ef803dda6d27d
Paris Monson <parismonson@yahoo.com>
2023-10-03 12:59:14 +0100

    Made LandingPage responsive

--- components/LandingPageBanner.jsx
+++ components/LandingPageBanner.jsx
@@ -1,30 +1,25 @@
 export function LandingPageBanner() {
-    return (
-      <div className="landing-banner flex flex-col h-[600px] mt-[72px] border-y-2">
-        <div className="border-2 flex h-[566px]">
-          <div className="w-2/3 border-2 flex flex-col justify-end p-5">
-            <div className="flex">
-            <p className="font-HKGroteskRegular text-8xl">
-            Ready to analyse some articles?
-            </p>
-            </div>
+  return (
+      <div className="landing-banner flex flex-col h-[600px] md:h-auto mt-[4.5rem] border-y-2">
+          <div className="border-2 flex flex-col md:flex-row h-full">
+              <div className="w-full md:w-2/3 border-2 flex flex-col justify-end p-5">
+                  <div className="flex flex-col">
+                      <p className="font-HKGroteskRegular md:text-8xl text-4xl">
+                          Ready to analyse some articles?
+                      </p>
+                      <div>
+                          <p className="md:text-lg text-base">
+                              Paste in any article you want and we will analyze it for any logical fallacies such as Ad Hominem, Strawman, Appeal to Authority, False Dichotomy, Slippery Slope, Confirmation Bias, Bandwagon 
+                          </p>
+                      </div>
+                  </div>
+              </div>
+              <div className="border-2 w-full md:w-1/3 flex justify-center items-center">
+                  <img src="openAiLogo.png" className="md:w-auto w-1/2" alt="OpenAI Logo"/>
+              </div>
           </div>
-          <div className="border-2 flex w-1/3">
-            b
-          </div>
-        </div>
-        <div className="flex flex-row border-2 justify-between p-1">
-          <div className="border-2 w-96 h-[64px] font-HKGroteskRegular">
-          Available from 18th October
-          </div>
-          <div className="border-2 w-96 h-[64px]">
-            2
-          </div>
-          <div className="border-2 w-96 h-[64px]">
-            3
-          </div>
-        </div>
       </div>
-    );
-  }
+  );
+}
+
   
